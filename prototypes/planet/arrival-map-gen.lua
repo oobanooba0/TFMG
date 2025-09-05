ee08796a999736d2af767dbc67d8b47b398b789e
oobanooba0 <108059652+oobanooba0@users.noreply.github.com>
2025-09-05 17:47:53 +1000

    Arrival decoratives added

--- prototypes/planet/arrival-map-gen.lua
+++ prototypes/planet/arrival-map-gen.lua
@@ -1,40 +1,204 @@
 data:extend{
   {
-    type = "noise-function",
-    name = "arrival_biome",
-    expression = [[
-        abs(multioctave_noise{
-            x = xx,
-            y = yy,
-            persistence = 0.5,
-            seed0 = map_seed,
-            seed1 = 1,
-            octaves = 5,
-            input_scale = 1/200,
-            output_scale = 1
-        })
-    ]],
-    parameters = {"xx", "yy"}
+	type = "noise-function",
+	name = "arrival_height",
+	expression = [[
+		clamp(abs(multioctave_noise{
+			x = xx,
+			y = yy,
+			persistence = 0.5,
+			seed0 = map_seed,
+			seed1 = 1,
+			octaves = 4,
+			input_scale = 1/200,
+			output_scale = 0.7
+		} + arrival_rough_noise(xx, yy)),0,1)
+	]],
+	parameters = {"xx", "yy"}
+  }
+}
+data:extend{
+	{
+	type = "noise-function",
+	name = "arrival_rough_noise",
+	expression = [[
+		multioctave_noise{
+			x = xx,
+			y = yy,
+			persistence = 0.7,
+			seed0 = map_seed,
+			seed1 = 2,
+			octaves = 6,
+			input_scale = 1/4,
+			output_scale = 0.07
+		}
+	]],
+	parameters = {"xx", "yy"}
   }
 }
 
-data.raw.tile["dust-flat"].autoplace = {
-    probability_expression = [[
-        arrival_biome(x, y) >= 0.6
-    ]],
-    order = "a[dust]-a[flat]"
+--Red Biome
+data.raw.tile["fulgoran-rock"].autoplace = {
+	probability_expression = [[
+	  arrival_height(x, y) - noise >= 1.05
+	]],
+	local_expressions = {
+  noise = [[
+		multioctave_noise{
+			x = x,
+			y = y,
+			persistence = 0.7,
+			seed0 = map_seed,
+			seed1 = 20,
+			octaves = 3,
+			input_scale = 1/3,
+			output_scale = 0.1
+		}
+	]],
+  },
+	order = "a[red]-a[dunes]"
+}
+
+data.raw.tile["fulgoran-dunes"].autoplace = {
+	probability_expression = [[
+	  arrival_height(x, y) - noise >= 0.95
+	]],
+	local_expressions = {
+  noise = [[
+		multioctave_noise{
+			x = x,
+			y = y,
+			persistence = 0.6,
+			seed0 = map_seed,
+			seed1 = 22,
+			octaves = 2,
+			input_scale = 1/12,
+			output_scale = 0.2
+		}
+	]],
+  },
+	order = "a[red]-b[sand]"
+}
+
+data.raw.tile["fulgoran-sand"].autoplace = {
+	probability_expression = [[
+	  arrival_height(x, y) - noise >= 0.9
+	]],
+	local_expressions = {
+  noise = [[
+		multioctave_noise{
+			x = x,
+			y = y,
+			persistence = 0.5,
+			seed0 = map_seed,
+			seed1 = 21,
+			octaves = 2,
+			input_scale = 1/30,
+			output_scale = 0.2
+		}
+	]],
+  },
+	order = "a[red]-c[rock]"
+}
+
+
+data.raw.tile["fulgoran-dust"].autoplace = {
+	probability_expression = [[
+		arrival_height(x, y) >= 0.85
+	]],
+	order = "a[red]-d[dust]"
+}
+
+--Dust Biome (midlands)
+data.raw.tile["dust-lumpy"].autoplace = {
+	probability_expression = [[
+		arrival_height(x, y) - noise >= 0.7
+	]],
+	local_expressions = {
+  noise = [[
+		multioctave_noise{
+			x = x,
+			y = y,
+			persistence = 0.7,
+			seed0 = map_seed,
+			seed1 = 13,
+			octaves = 2,
+			input_scale = 1/8,
+			output_scale = 0.3
+		}
+	]],
+  },
+	order = "c[dust]-a[lumpy]"
 }
 
 data.raw.tile["dust-crests"].autoplace = {
-    probability_expression = [[
-        arrival_biome(x, y) >= 0.5
-    ]],
-    order = "a[dust]-b[crests]"
+	probability_expression = [[
+		arrival_height(x, y) - noise >= 0.7
+	]],
+	local_expressions = {
+  noise = [[
+		multioctave_noise{
+			x = x,
+			y = y,
+			persistence = 0.7,
+			seed0 = map_seed,
+			seed1 = 11,
+			octaves = 2,
+			input_scale = 1/8,
+			output_scale = 0.3
+		}
+	]],
+  },
+	order = "c[dust]-b[crests]"
+}
+data.raw.tile["dust-patchy"].autoplace = {
+	probability_expression = [[
+  arrival_height(x, y) - noise >= 0.4
+	]],
+	local_expressions = {
+  noise = [[
+		multioctave_noise{
+			x = x,
+			y = y,
+			persistence = 0.8,
+			seed0 = map_seed,
+			seed1 = 12,
+			octaves = 3,
+			input_scale = 1/30,
+			output_scale = 0.2
+		}
+	]],
+  },
+	order = "c[dust]-c[patchy]"
 }
 
-data.raw.tile["fulgoran-dust"].autoplace = {
-    probability_expression = [[
-        arrival_biome(x, y) >= 0.1
-    ]],
-    order = "b[Red]-a[flat]"
+data.raw.tile["dust-flat"].autoplace = {
+	probability_expression = [[
+		arrival_height(x, y) >= 0
+	]],
+	order = "c[dust]-d[flat]"
 }
+--ice biome
+
+data.raw.tile["ice-smooth"].autoplace = {
+	probability_expression = [[
+		arrival_height(x, y) - noise <= -0.1
+	]],
+	local_expressions = {
+  noise = [[
+		multioctave_noise{
+			x = x,
+			y = y,
+			persistence = 0.2,
+			seed0 = map_seed,
+			seed1 = 30,
+			octaves = 2,
+			input_scale = 1/100,
+			output_scale = 0.2
+		}
+	]],
+  },
+	order = "b[ice]-a[flat]"
+}
+
+--data.raw.entity["cliff"]
--- prototypes/planet/planet_map_gen.lua
+++ prototypes/planet/planet_map_gen.lua
@@ -8,21 +8,51 @@ planet_map_gen.arrival = function()
       ["tile"] =
       {
         settings =
-        {          
+        {
           ["dust-flat"] = {},
           ["dust-crests"] = {},
-          --["dust-patchy"] = {},
-          --["dust-lumpy"] = {},
-          --["fulgoran-dunes"] = {},
-          --["fulgoran-sand"] = {},
-          --["fulgoran-rock"] = {},
+          ["dust-patchy"] = {},
+          ["dust-lumpy"] = {},
+          ["fulgoran-dunes"] = {},
           ["fulgoran-dust"] = {},
+          ["fulgoran-sand"] = {},
+          ["fulgoran-rock"] = {},
+          ["ice-smooth"] = {},
           --["snow-flat"] = {},
           --["snow-crests"] = {},
           --["snow-lumpy"] = {},
           --["snow-patchy"] = {},
         }
       },
+      ["decorative"] =
+      {
+        settings =
+        {
+          --["vulcanus-rock-decal-large"] = {},
+          --["vulcanus-crack-decal-large"] = {},
+          ["vulcanus-dune-decal"] = {},
+          --["vulcanus-sand-decal"] = {},
+          ["calcite-stain"] = {},
+          ["calcite-stain-small"] = {},
+          ["crater-small"] = {},
+          ["crater-large"] = {},
+          ["pumice-relief-decal"] = {},
+          ["small-volcanic-rock"] = {},
+          --["medium-volcanic-rock"] = {},
+          --["tiny-volcanic-rock"] = {},
+          ["waves-decal"] = {},
+          ["medium-fulgora-rock"] = {},
+          ["small-fulgora-rock"] = {},
+          ["tiny-fulgora-rock"] = {},
+
+        }
+      },
+      ["entity"] =
+      {
+        settings =
+        {
+        }
+      }
     }
   }
 end
