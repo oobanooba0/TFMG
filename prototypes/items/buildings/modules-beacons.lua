3359217304f7f50d85fffc05a157f9f02532ae00
oobanooba0 <108059652+oobanooba0@users.noreply.github.com>
2025-10-04 16:46:14 +1000

    module adjustment

--- prototypes/items/buildings/modules-beacons.lua
+++ prototypes/items/buildings/modules-beacons.lua
@@ -1,6 +1,6 @@
 local item_sounds = require("__base__.prototypes.item_sounds")
 local speed_module = data.raw.module["speed-module"]
-speed_module.effect = {speed = 0.2, consumption = 0.5, quality = -0.1}
+speed_module.effect = {speed = 0.2, consumption = 0.6, quality = -0.1}
 speed_module.icon = "__base__/graphics/icons/speed-module-3.png"
 
 local efficiency_module = data.raw.module["efficiency-module"]
