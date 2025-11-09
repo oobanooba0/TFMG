3c50106c33c5c22c393166d53b5b2b950e9a5caf
oobanooba0 <108059652+oobanooba0@users.noreply.github.com>
2025-11-09 15:29:23 +1100

    FIELDS Soundtrack added

--- sound/ambient/ambient-sounds.lua
+++ sound/ambient/ambient-sounds.lua
@@ -1,5 +1,12 @@
 data.raw ['ambient-sound']['main-menu'] = nil--to boldly delete the vanilla main menu music
 
+
+for _,track in pairs(data.raw ['ambient-sound']) do --removing the vanilla nauvis tracks. Normally I'd delete everything but space still needs some music that isnt just one track.
+  if track.planet == "nauvis" then 
+    data.raw['ambient-sound'][track.name] = nil 
+  end
+end
+
 data:extend({
 --main menu
   {
@@ -12,106 +19,120 @@ data:extend({
     }
   },
 })
-data:extend({--arrival
----main
-  {
-    type = "ambient-sound",
-    name = "ENTROPY",
-    track_type = "main-track",
-    planet = "nauvis",
-    sound = {
-      filename = "__TFMG-music__/arrival/ENTROPY.ogg",
-      volume = 1,
+data:extend({
+--Space
+  --main
+    {
+      type = "ambient-sound",
+      name = "FIELDS",
+      track_type = "main-track",
+      sound = {
+        filename = "__TFMG-music__/space/FIELDS.ogg",
+        volume = 1,
+      },
+      weight = 10
     },
-    weight = 10
-  },
-  {
-    type = "ambient-sound",
-    name = "LIFE",
-    track_type = "main-track",
-    planet = "nauvis",
-    sound = {
-      filename = "__TFMG-music__/arrival/LIFE.ogg",
-      volume = 1,
+
+--arrival
+  --main
+    {
+      type = "ambient-sound",
+      name = "ENTROPY",
+      track_type = "main-track",
+      planet = "nauvis",
+      sound = {
+        filename = "__TFMG-music__/arrival/ENTROPY.ogg",
+        volume = 1,
+      },
+      weight = 10
     },
-    weight = 10
-  },
-  {
-    type = "ambient-sound",
-    name = "TREMOR",
-    track_type = "main-track",
-    planet = "nauvis",
-    sound = {
-      filename = "__TFMG-music__/arrival/TREMOR.ogg",
-      volume = 1,
+    {
+      type = "ambient-sound",
+      name = "LIFE",
+      track_type = "main-track",
+      planet = "nauvis",
+      sound = {
+        filename = "__TFMG-music__/arrival/LIFE.ogg",
+        volume = 1,
+      },
+      weight = 10
     },
-    weight = 10
-  },
----interludes
-  {
-    type = "ambient-sound",
-    name = "Clock-Interlude",
-    track_type = "interlude",
-    planet = "nauvis",
-    sound = {
-      filename = "__TFMG-music__/arrival/Clock-Interlude.ogg",
-      volume = 0.7,
+    {
+      type = "ambient-sound",
+      name = "TREMOR",
+      track_type = "main-track",
+      planet = "nauvis",
+      sound = {
+        filename = "__TFMG-music__/arrival/TREMOR.ogg",
+        volume = 1,
+      },
+      weight = 10
     },
-    weight = 10
-  },
-  {
-    type = "ambient-sound",
-    name = "Count-Interlude",
-    track_type = "interlude",
-    planet = "nauvis",
-    sound = {
-      filename = "__TFMG-music__/arrival/Count-Interlude.ogg",
-      volume = 0.7,
+  --interludes
+    {
+      type = "ambient-sound",
+      name = "Clock-Interlude",
+      track_type = "interlude",
+      planet = "nauvis",
+      sound = {
+        filename = "__TFMG-music__/arrival/Clock-Interlude.ogg",
+        volume = 0.7,
+      },
+      weight = 10
     },
-    weight = 10
-  },
-  {
-    type = "ambient-sound",
-    name = "Spider-Steps-Interlude",
-    track_type = "interlude",
-    planet = "nauvis",
-    sound = {
-      filename = "__TFMG-music__/arrival/Spider-Steps-Interlude.ogg",
-      volume = 0.7,
+    {
+      type = "ambient-sound",
+      name = "Count-Interlude",
+      track_type = "interlude",
+      planet = "nauvis",
+      sound = {
+        filename = "__TFMG-music__/arrival/Count-Interlude.ogg",
+        volume = 0.7,
+      },
+      weight = 10
     },
-    weight = 10
-  },
-  {
-    type = "ambient-sound",
-    name = "Wyrms-Interlude",
-    track_type = "interlude",
-    planet = "nauvis",
-    sound = {
-      filename = "__TFMG-music__/arrival/Wyrms-Interlude.ogg",
-      volume = 0.7,
+    {
+      type = "ambient-sound",
+      name = "Spider-Steps-Interlude",
+      track_type = "interlude",
+      planet = "nauvis",
+      sound = {
+        filename = "__TFMG-music__/arrival/Spider-Steps-Interlude.ogg",
+        volume = 0.7,
+      },
+      weight = 10
     },
-    weight = 10
-  },
-  {
-    type = "ambient-sound",
-    name = "Reflections-Interlude",
-    track_type = "interlude",
-    planet = "nauvis",
-    sound = {
-      filename = "__TFMG-music__/arrival/Reflections-Interlude.ogg",
-      volume = 0.7,
+    {
+      type = "ambient-sound",
+      name = "Wyrms-Interlude",
+      track_type = "interlude",
+      planet = "nauvis",
+      sound = {
+        filename = "__TFMG-music__/arrival/Wyrms-Interlude.ogg",
+        volume = 0.7,
+      },
+      weight = 10
     },
-    weight = 10
-  },
-  {
-    type = "ambient-sound",
-    name = "Decomposition-Interlude",
-    track_type = "interlude",
-    planet = "nauvis",
-    sound = {
-      filename = "__TFMG-music__/arrival/Decomposition-Interlude.ogg",
-      volume = 0.7,
+    {
+      type = "ambient-sound",
+      name = "Reflections-Interlude",
+      track_type = "interlude",
+      planet = "nauvis",
+      sound = {
+        filename = "__TFMG-music__/arrival/Reflections-Interlude.ogg",
+        volume = 0.7,
+      },
+      weight = 10
     },
-    weight = 10
-  },
+    {
+      type = "ambient-sound",
+      name = "Decomposition-Interlude",
+      track_type = "interlude",
+      planet = "nauvis",
+      sound = {
+        filename = "__TFMG-music__/arrival/Decomposition-Interlude.ogg",
+        volume = 0.7,
+      },
+      weight = 10
+    },
 })
