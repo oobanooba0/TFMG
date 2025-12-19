ade6b031809d737d2f4420b19236c60ef0d974c6
oobanooba0 <108059652+oobanooba0@users.noreply.github.com>
2025-12-19 16:41:16 +1100

    some locale fixes

--- prototypes/buildings/inserters.lua
+++ prototypes/buildings/inserters.lua
@@ -4,6 +4,8 @@ local inserter_1_rotation_speed = 1/60
 --in tiles per second?
 local inserter_1_extension_speed = 2/60
 
+local long_arm_speed_multiplier = 2/3
+
 --remember that revolutions per second =/= actions per second. Actual speeds vary depending on circumstances.
 
 local hit_effects = require ("__base__.prototypes.entity.hit-effects")
@@ -14,5 +16,5 @@ inserter_1.rotation_speed = inserter_1_rotation_speed
 inserter_1.extension_speed = inserter_1_extension_speed
 
 local long_inserter_1 = data.raw["inserter"]["long-handed-inserter"]
-long_inserter_1.rotation_speed = inserter_1_rotation_speed*0.5
-long_inserter_1.extension_speed = inserter_1_extension_speed*0.5
+long_inserter_1.rotation_speed = inserter_1_rotation_speed*long_arm_speed_multiplier
+long_inserter_1.extension_speed = inserter_1_extension_speed*long_arm_speed_multiplier
