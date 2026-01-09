1f4410362cf7fb292767a29078fb827fb60d57fe
oobanooba0 <108059652+oobanooba0@users.noreply.github.com>
2026-01-09 15:54:14 +1100

    field space location and route

--- prototypes/planet/planet.lua
+++ prototypes/planet/planet.lua
@@ -2,8 +2,14 @@ local planet_map_gen = require("__TFMG__.prototypes.planet.planet_map_gen")
 local asteroid_util = require("__TFMG__.prototypes.planet.asteroid-spawn-definitions")
 local effects = require("__core__/lualib/surface-render-parameter-effects")
 local procession_graphic_catalogue_types = require("__base__/prototypes/planet/procession-graphic-catalogue-types")
+
 local arrival_distance = 70
 local arrival_orientation = 0.275
+
+local field_distance = 50
+local field_orientation = 0.25
+
+
 data:extend({--surface properties
   {
     type = "surface-property",
@@ -52,6 +58,106 @@ data:extend({--surface properties
     --Asteroid code
     asteroid_spawn_influence = 1,
     asteroid_spawn_definitions = asteroid_util.spawn_definitions(asteroid_util.arrival_near_void, 0.1),
+    persistent_ambient_sounds =
+    {
+      base_ambience = {filename = "__space-age__/sound/wind/base-wind-aquilo.ogg", volume = 0.5},
+      wind = {filename = "__space-age__/sound/wind/wind-aquilo.ogg", volume = 0.8},
+      crossfade =
+      {
+        order = {"wind", "base_ambience"},
+        curve_type = "cosine",
+        from = {control = 0.35, volume_percentage = 0.0},
+        to = {control = 2, volume_percentage = 100.0}
+      },
+      semi_persistent =
+      {
+        {
+          sound =
+          {
+            variations = sound_variations("__space-age__/sound/world/semi-persistent/ice-cracks", 5, 0.7),
+            advanced_volume_control =
+            {
+              fades = {fade_in = {curve_type = "cosine", from = {control = 0.5, volume_percentage = 0.0}, to = {2, 100.0}}}
+            }
+          },
+          delay_mean_seconds = 10,
+          delay_variance_seconds = 5
+        },
+        {
+          sound = {variations = sound_variations("__space-age__/sound/world/semi-persistent/cold-wind-gust", 5, 0.3)},
+          delay_mean_seconds = 15,
+          delay_variance_seconds = 9
+        }
+      }
+    },
+    procession_graphic_catalogue =
+    {
+      {
+        index = procession_graphic_catalogue_types.planet_hatch_emission_in_1,
+        sprite = util.sprite_load("__base__/graphics/entity/cargo-hubs/hatches/planet-lower-hatch-pod-emission-A",
+        {
+          priority = "medium",
+          draw_as_glow = true,
+          blend_mode = "additive",
+          scale = 0.5,
+          shift = util.by_pixel(-16, 96) --32 x ({0.5, -3.5} + {0, 0.5})
+        })
+      },
+      {
+        index = procession_graphic_catalogue_types.planet_hatch_emission_in_2,
+        sprite = util.sprite_load("__base__/graphics/entity/cargo-hubs/hatches/planet-lower-hatch-pod-emission-B",
+        {
+          priority = "medium",
+          draw_as_glow = true,
+          blend_mode = "additive",
+          scale = 0.5,
+          shift = util.by_pixel(-64, 96) --32 x ({2, -3.5} + {0, 0.5})
+        })
+      },
+      {
+        index = procession_graphic_catalogue_types.planet_hatch_emission_in_3,
+        sprite = util.sprite_load("__base__/graphics/entity/cargo-hubs/hatches/planet-lower-hatch-pod-emission-C",
+        {
+          priority = "medium",
+          draw_as_glow = true,
+          blend_mode = "additive",
+          scale = 0.5,
+          shift = util.by_pixel(-40, 64) --32 x ({1.25, -2.5} + {0, 0.5})
+        })
+      }
+    }
+  },
+  {
+    type = "planet",
+    name = "field",
+    icon = "__TFMG-assets-0__/icons/planets/arrival.png",
+    starmap_icon = "__TFMG-assets-0__/icons/planets/arrival-starmap.png",
+    starmap_icon_size = 2048,
+    gravity_pull = 10,
+    distance = field_distance,
+    orientation = field_orientation,
+    magnitude = 1,
+    order = "c[field]",
+    subgroup = "planets",
+    map_seed_offset = 1000,
+    map_gen_settings = planet_map_gen.arrival(),
+    pollutant_type = nil,
+    solar_power_in_space = 100,
+    planet_procession_set =
+    {
+      arrival = {"default-b"},
+      departure = {"default-rocket-a"}
+    },
+    surface_properties =
+    {
+      ["day-night-cycle"] = 7 * minute,
+      ["solar-power"] = 100,
+      pressure = 1,
+      gravity = 0.1,
+    },
+    --Asteroid code
+    asteroid_spawn_influence = 1,
+    asteroid_spawn_definitions = asteroid_util.spawn_definitions(asteroid_util.limit_field, 0.9),
     persistent_ambient_sounds =
     {
       base_ambience = {filename = "__space-age__/sound/wind/base-wind-aquilo.ogg", volume = 0.5},
@@ -180,6 +286,16 @@ data:extend({--surface properties
     order = "a",
     length = 50000,
     asteroid_spawn_definitions = asteroid_util.spawn_definitions(asteroid_util.arrival_limit),
+  },
+  {
+    type = "space-connection",
+    name = "limit-field",
+    subgroup = "planet-connections",
+    from = "limit",
+    to = "field",
+    order = "b",
+    length = 200000,
+    asteroid_spawn_definitions = asteroid_util.spawn_definitions(asteroid_util.limit_field),
   },
 --pollution types
 })
